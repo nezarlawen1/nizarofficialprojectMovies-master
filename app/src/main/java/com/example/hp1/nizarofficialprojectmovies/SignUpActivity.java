bb028380c813bcc26769e0170d5aee3a401eaacd
nezarlawen1 <nezar.lawen@gmail.com>
2018-10-11 22:33:07 +0300

    sign up fixed and done ,but there is a compiler error

--- app/src/main/java/com/example/hp1/nizarofficialprojectmovies/SignUpActivity.java
+++ app/src/main/java/com/example/hp1/nizarofficialprojectmovies/SignUpActivity.java
@@ -9,7 +9,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
