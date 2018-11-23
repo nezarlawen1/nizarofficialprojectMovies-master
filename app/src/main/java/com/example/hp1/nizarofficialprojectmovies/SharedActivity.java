890924382f158df6d6ec63392ca4e92630b316b3
nezarlawen1 <nezar.lawen@gmail.com>
2018-11-23 11:11:31 +0200

    shared pref

---
+++ app/src/main/java/com/example/hp1/nizarofficialprojectmovies/SharedActivity.java
@@ -0,0 +1,45 @@
+package com.example.hp1.nizarofficialprojectmovies;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.StringReader;
+
+public class SharedActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btSave;
+    EditText etNamne;
+    TextView tvName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared);
+
+
+
+        btSave = (Button) findViewById(R.id.btSave);
+        btSave.setOnClickListener(this);
+        etNamne = (EditText) findViewById(R.id.etName);
+
+        SharedPreferences pref = getSharedPreferences("Profile",MODE_PRIVATE);
+        String name=pref.getString("name",null);
+
+        tvName = (TextView) findViewById(R.id.tvName);
+        if (name !=null){
+            tvName.setText(name);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        SharedPreferences pref = getSharedPreferences("Profile",MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("name",etNamne.getText().toString());
+        editor.commit();
+    }
+}
