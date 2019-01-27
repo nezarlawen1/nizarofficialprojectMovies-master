faa7c3ca30e927fa7cb1ec5e16c48e57977f569a
nezarlawen1 <nezar.lawen@gmail.com>
2019-01-27 20:08:44 +0200

    TRYING API done

--- app/src/main/java/com/example/hp1/nizarofficialprojectmovies/Main3Activity.java
+++ app/src/main/java/com/example/hp1/nizarofficialprojectmovies/Main3Activity.java
@@ -2,13 +2,57 @@ package com.example.hp1.nizarofficialprojectmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Main3Activity extends AppCompatActivity {
 
     public static String BASE_URL = "https://api.themoviedb.org";
+    public static int PAGE = 1;
+    public static String API_KEY = "efc31ce25ae8abcffb42bd206ad17519";
+    public static String LANGUAGE = "en-US";
+    public static  String CATEGORY = "popular";
+
+    private TextView myTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        myTextView = (TextView)findViewById(R.id.my_tv);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Api_Interface myInterface = retrofit.create(Api_Interface.class);
+
+        Call<MoviesResult> call = myInterface.listofMovies(CATEGORY,API_KEY,LANGUAGE,PAGE);
+
+        call.enqueue(new Callback<MoviesResult>() {
+            @Override
+            public void onResponse(Call<MoviesResult> call, Response<MoviesResult> response) {
+                MoviesResult results = response.body();
+                List<MoviesResult.ResultsBean> listofMovies = results.getResults();
+                MoviesResult.ResultsBean firstMovie = listofMovies.get(0);
+
+                myTextView.setText(firstMovie.getTitle());
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResult> call, Throwable t) {
+                t.printStackTrace();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hp1/nizarofficialprojectmovies/MoviesResult.java
+++ app/src/main/java/com/example/hp1/nizarofficialprojectmovies/MoviesResult.java
@@ -1,6 +1,197 @@
 package com.example.hp1.nizarofficialprojectmovies;
 
+import java.util.List;
+
 public class MoviesResult {
 
-    
+
+    /**
+     * page : 1
+     * total_results : 19852
+     * total_pages : 993
+     * results : [{"vote_count":2980,"id":297802,"video":false,"vote_average":6.9,"title":"Aquaman","popularity":410.77,"poster_path":"/5Kg76ldv7VxeX9YlcQXiowHgdX6.jpg","original_language":"en","original_title":"Aquaman","genre_ids":[28,14,878,12],"backdrop_path":"/9QusGjxcYvfPD1THg6oW3RLeNn7.jpg","adult":false,"overview":"Arthur Curry learns that he is the heir to the underwater kingdom of Atlantis, and must step forward to lead his people and be a hero to the world.","release_date":"2018-12-07"},{"vote_count":958,"id":424783,"video":false,"vote_average":6.5,"title":"Bumblebee","popularity":380.038,"poster_path":"/fw02ONlDhrYjTSZV8XO6hhU3ds3.jpg","original_language":"en","original_title":"Bumblebee","genre_ids":[28,12,878],"backdrop_path":"/hMANgfPHR1tRObNp2oPiOi9mMlz.jpg","adult":false,"overview":"On the run in the year 1987, Bumblebee finds refuge in a junkyard in a small Californian beach town. Charlie, on the cusp of turning 18 and trying to find her place in the world, discovers Bumblebee, battle-scarred and broken.  When Charlie revives him, she quickly learns this is no ordinary yellow VW bug.","release_date":"2018-12-15"},{"vote_count":4038,"id":424694,"video":false,"vote_average":8.2,"title":"Bohemian Rhapsody","popularity":339.347,"poster_path":"/gbmkFWdtihe1VfydTDsieQ6VfGL.jpg","original_language":"en","original_title":"Bohemian Rhapsody","genre_ids":[18,10402],"backdrop_path":"/93xA62uLd5CwMOAs37eQ7vPc1iV.jpg","adult":false,"overview":"Singer Freddie Mercury, guitarist Brian May, drummer Roger Taylor and bass guitarist John Deacon take the music world by storm when they form the rock 'n' roll band Queen in 1970. Hit songs become instant classics. When Mercury's increasingly wild lifestyle starts to spiral out of control, Queen soon faces its greatest challenge yet \u2013 finding a way to keep the band together amid the success and excess.","release_date":"2018-10-24"},{"vote_count":744,"id":480530,"video":false,"vote_average":6.6,"title":"Creed II","popularity":251.029,"poster_path":"/v3QyboWRoA4O9RbcsqH8tJMe8EB.jpg","original_language":"en","original_title":"Creed II","genre_ids":[18,28],"backdrop_path":"/8yqLPNwNCtpOPc3XkOlkSMnghzw.jpg","adult":false,"overview":"Follows Adonis Creed's life inside and outside of the ring as he deals with new found fame, issues with his family, and his continuing quest to become a champion.","release_date":"2018-11-21"},{"vote_count":596,"id":428078,"video":false,"vote_average":6,"title":"Mortal Engines","popularity":247.183,"poster_path":"/gLhYg9NIvIPKVRTtvzCWnp1qJWG.jpg","original_language":"en","original_title":"Mortal Engines","genre_ids":[878,28,12,14,53],"backdrop_path":"/rxYG6Sj95as9rv9wKIHUx6ATWd3.jpg","adult":false,"overview":"Set in a world many thousands of years in the future. Earth\u2019s cities now roam the globe on huge wheels, devouring each other in a struggle for ever diminishing resources. On one of these massive Traction Cities, Tom Natsworthy has an unexpected encounter with a mysterious young woman from the Outlands who will change the course of his life forever.","release_date":"2018-12-05"},{"vote_count":2913,"id":332562,"video":false,"vote_average":7.5,"title":"A Star Is Born","popularity":201.685,"poster_path":"/wrFpXMNBRj2PBiN4Z5kix51XaIZ.jpg","original_language":"en","original_title":"A Star Is Born","genre_ids":[18,10402,10749],"backdrop_path":"/wqtaHWOEZ3rXDJ8c6ZZShulbo18.jpg","adult":false,"overview":"Seasoned musician Jackson Maine discovers\u2014and falls in love with\u2014struggling artist Ally. She has just about given up on her dream to make it big as a singer\u2014until Jack coaxes her into the spotlight. But even as Ally's career takes off, the personal side of their relationship is breaking down, as Jack fights an ongoing battle with his own internal demons.","release_date":"2018-10-03"},{"vote_count":241,"id":504172,"video":false,"vote_average":6.2,"title":"The Mule","popularity":190.11,"poster_path":"/t0idiLMalKMj2pLsvqHrOM4LPdQ.jpg","original_language":"en","original_title":"The Mule","genre_ids":[80,18,53],"backdrop_path":"/bkc4AY6FTa3yNqrshE9b1elDzPt.jpg","adult":false,"overview":"A 90 year old horticulturalist and Korean War veteran is caught transporting $3 million worth of cocaine through Illinois for a Mexican drug cartel.","release_date":"2018-12-14"},{"vote_count":447,"id":375588,"video":false,"vote_average":5.8,"title":"Robin Hood","popularity":183.423,"poster_path":"/AiRfixFcfTkNbn2A73qVJPlpkUo.jpg","original_language":"en","original_title":"Robin Hood","genre_ids":[12,28,53],"backdrop_path":"/AuA50D7T7S7OEVcGo0ZKaMhERn0.jpg","adult":false,"overview":"A war-hardened Crusader and his Moorish commander mount an audacious revolt against the corrupt English crown.","release_date":"2018-11-20"},{"vote_count":654,"id":360920,"video":false,"vote_average":6.3,"title":"The Grinch","popularity":179.839,"poster_path":"/gpkHvkCtZOeCQ2DelnJ2LB1WjZ5.jpg","original_language":"en","original_title":"The Grinch","genre_ids":[16,10751,35,14],"backdrop_path":"/juc9wt7Eh2IarLL5S1yQ1a21O1A.jpg","adult":false,"overview":"The Grinch hatches a scheme to ruin Christmas when the residents of Whoville plan their annual holiday celebration.","release_date":"2018-11-08"},{"vote_count":13,"id":390634,"video":false,"vote_average":4.8,"title":"Fate/stay night: Heaven\u2019s Feel II. lost butterfly","popularity":171.319,"poster_path":"/t0jwlwFUrtkPJoq5JH9YrRVROXZ.jpg","original_language":"ja","original_title":"劇場版「Fate/stay night [Heaven's Feel] ⅠⅠ. lost butterfly」","genre_ids":[16,28,14,12],"backdrop_path":"/3haESpzscyP1pLl2BxQ9j5KK9cM.jpg","adult":false,"overview":"Theatrical-release adaptation of the visual novel \"Fate/stay night\", following the third and final route. (Part 2 of a trilogy.)","release_date":"2019-01-12"},{"vote_count":722,"id":450465,"video":false,"vote_average":6.9,"title":"Glass","popularity":161.27,"poster_path":"/svIDTNUoajS8dLEo7EosxvyAsgJ.jpg","original_language":"en","original_title":"Glass","genre_ids":[53,27,9648,18],"backdrop_path":"/ngBFDOsx13sFXiMweDoL54XYknR.jpg","adult":false,"overview":"In a series of escalating encounters, security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.","release_date":"2019-01-16"},{"vote_count":3,"id":568709,"video":false,"vote_average":6.3,"title":"Preman Pensiun","popularity":160.389,"poster_path":"/7xfWyoz4SF5LHZ713eMtC2aZ0lT.jpg","original_language":"id","original_title":"Preman Pensiun","genre_ids":[35,18],"backdrop_path":"/rrznEEdi4OVPsrqbEgmCE7nzjPs.jpg","adult":false,"overview":"","release_date":"2019-01-17"},{"vote_count":55,"id":522681,"video":false,"vote_average":6.9,"title":"Escape Room","popularity":159.961,"poster_path":"/8yZAx7tlKRZIg7pJfaPhl00yHIQ.jpg","original_language":"en","original_title":"Escape Room","genre_ids":[27,53,28],"backdrop_path":"/9cmrRktcAmhteKMNRl2hU6rQzLJ.jpg","adult":false,"overview":"Six strangers find themselves in circumstances beyond their control, and must use their wits to survive.","release_date":"2019-01-03"},{"vote_count":253,"id":503314,"video":false,"vote_average":7.5,"title":"Dragon Ball Super: Broly","popularity":149.24,"poster_path":"/f03YksE4NggUjG75toz4H1YAGRf.jpg","original_language":"ja","original_title":"ドラゴンボール超スーパー ブロリー","genre_ids":[28,16,878],"backdrop_path":"/6OTRuxpwUUGbmCX3MKP25dOmo59.jpg","adult":false,"overview":"Earth is peaceful following the Tournament of Power. Realizing that the universes still hold many more strong people yet to see, Goku spends all his days training to reach even greater heights. Then one day, Goku and Vegeta are faced by a Saiyan called 'Broly' who they've never seen before. The Saiyans were supposed to have been almost completely wiped out in the destruction of Planet Vegeta, so what's this one doing on Earth? This encounter between the three Saiyans who have followed completely different destinies turns into a stupendous battle, with even Frieza (back from Hell) getting caught up in the mix.","release_date":"2018-12-14"},{"vote_count":228,"id":446807,"video":false,"vote_average":5.8,"title":"The Girl in the Spider's Web","popularity":146.561,"poster_path":"/w4ibr8R702DCjwYniry1D1XwQXj.jpg","original_language":"en","original_title":"The Girl in the Spider's Web","genre_ids":[18,53,9648,80,28],"backdrop_path":"/r0ljJIPBGsGdpeDdlHpzCiNnxiB.jpg","adult":false,"overview":"In Stockholm, Sweden, hacker Lisbeth Salander is hired by Frans Balder, a computer engineer, to retrieve a program that he believes it is too dangerous to exist.","release_date":"2018-10-26"},{"vote_count":1320,"id":324857,"video":false,"vote_average":8.5,"title":"Spider-Man: Into the Spider-Verse","popularity":142.975,"poster_path":"/laMM4lpQSh5z6KIBPwWogkjzBVQ.jpg","original_language":"en","original_title":"Spider-Man: Into the Spider-Verse","genre_ids":[28,12,16,878,35],"backdrop_path":"/7d6EY00g1c39SGZOoCJ5Py9nNth.jpg","adult":false,"overview":"Miles Morales is juggling his life between being a high school student and being a spider-man. When Wilson \"Kingpin\" Fisk uses a super collider, others from across the Spider-Verse are transported to this dimension.","release_date":"2018-12-07"},{"vote_count":705,"id":400650,"video":false,"vote_average":6.8,"title":"Mary Poppins Returns","popularity":134.737,"poster_path":"/uTVGku4LibMGyKgQvjBtv3OYfAX.jpg","original_language":"en","original_title":"Mary Poppins Returns","genre_ids":[14,10751,35],"backdrop_path":"/cwiJQXezWz876K3jS57Sq56RYCZ.jpg","adult":false,"overview":"In Depression-era London, a now-grown Jane and Michael Banks, along with Michael's three children, are visited by the enigmatic Mary Poppins following a personal loss. Through her unique magical skills, and with the aid of her friend Jack, she helps the family rediscover the joy and wonder missing in their lives.","release_date":"2018-12-19"},{"vote_count":368,"id":401469,"video":false,"vote_average":6.7,"title":"Widows","popularity":132.122,"poster_path":"/tvmPiTShsfp4vSUBFsCHYaX9M7i.jpg","original_language":"en","original_title":"Widows","genre_ids":[80,18,53],"backdrop_path":"/71OjxI27tK7kTIiPnMkdQDx14pe.jpg","adult":false,"overview":"A police shootout leaves four thieves dead during an explosive armed robbery attempt in Chicago. Their widows have nothing in common except a debt left behind by their spouses' criminal activities. Hoping to forge a future on their own terms, they join forces to pull off a heist.","release_date":"2018-11-06"},{"vote_count":3066,"id":338952,"video":false,"vote_average":6.9,"title":"Fantastic Beasts: The Crimes of Grindelwald","popularity":130.176,"poster_path":"/kQKcbJ9uYkTQql2R8L4jTUz7l90.jpg","original_language":"en","original_title":"Fantastic Beasts: The Crimes of Grindelwald","genre_ids":[10751,14,12],"backdrop_path":"/wDN3FIcQQ1HI7mz1OOKYHSQtaiE.jpg","adult":false,"overview":"Gellert Grindelwald has escaped imprisonment and has begun gathering followers to his cause\u2014elevating wizards above all non-magical beings. The only one capable of putting a stop to him is the wizard he once called his closest friend, Albus Dumbledore. However, Dumbledore will need to seek help from the wizard who had thwarted Grindelwald once before, his former student Newt Scamander, who agrees to help, unaware of the dangers that lie ahead. Lines are drawn as love and loyalty are tested, even among the truest friends and family, in an increasingly divided wizarding world.","release_date":"2018-11-14"},{"vote_count":326,"id":399402,"video":false,"vote_average":5.9,"title":"Hunter Killer","popularity":123.066,"poster_path":"/a0j18XNVhP4RcW3wXwsqT0kVoQm.jpg","original_language":"en","original_title":"Hunter Killer","genre_ids":[28,53],"backdrop_path":"/gCIB3DdK00DyfwosVGf7pnFFdK3.jpg","adult":false,"overview":"An untested American submarine captain teams with U.S. Navy SEALs to rescue the Russian president, who has been kidnapped by a rogue general.","release_date":"2018-10-19"}]
+     */
+
+    private int page;
+    private int total_results;
+    private int total_pages;
+    private List<ResultsBean> results;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public List<ResultsBean> getResults() {
+        return results;
+    }
+
+    public void setResults(List<ResultsBean> results) {
+        this.results = results;
+    }
+
+    public static class ResultsBean {
+        /**
+         * vote_count : 2980
+         * id : 297802
+         * video : false
+         * vote_average : 6.9
+         * title : Aquaman
+         * popularity : 410.77
+         * poster_path : /5Kg76ldv7VxeX9YlcQXiowHgdX6.jpg
+         * original_language : en
+         * original_title : Aquaman
+         * genre_ids : [28,14,878,12]
+         * backdrop_path : /9QusGjxcYvfPD1THg6oW3RLeNn7.jpg
+         * adult : false
+         * overview : Arthur Curry learns that he is the heir to the underwater kingdom of Atlantis, and must step forward to lead his people and be a hero to the world.
+         * release_date : 2018-12-07
+         */
+
+        private int vote_count;
+        private int id;
+        private boolean video;
+        private double vote_average;
+        private String title;
+        private double popularity;
+        private String poster_path;
+        private String original_language;
+        private String original_title;
+        private String backdrop_path;
+        private boolean adult;
+        private String overview;
+        private String release_date;
+        private List<Integer> genre_ids;
+
+        public int getVote_count() {
+            return vote_count;
+        }
+
+        public void setVote_count(int vote_count) {
+            this.vote_count = vote_count;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public boolean isVideo() {
+            return video;
+        }
+
+        public void setVideo(boolean video) {
+            this.video = video;
+        }
+
+        public double getVote_average() {
+            return vote_average;
+        }
+
+        public void setVote_average(double vote_average) {
+            this.vote_average = vote_average;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public double getPopularity() {
+            return popularity;
+        }
+
+        public void setPopularity(double popularity) {
+            this.popularity = popularity;
+        }
+
+        public String getPoster_path() {
+            return poster_path;
+        }
+
+        public void setPoster_path(String poster_path) {
+            this.poster_path = poster_path;
+        }
+
+        public String getOriginal_language() {
+            return original_language;
+        }
+
+        public void setOriginal_language(String original_language) {
+            this.original_language = original_language;
+        }
+
+        public String getOriginal_title() {
+            return original_title;
+        }
+
+        public void setOriginal_title(String original_title) {
+            this.original_title = original_title;
+        }
+
+        public String getBackdrop_path() {
+            return backdrop_path;
+        }
+
+        public void setBackdrop_path(String backdrop_path) {
+            this.backdrop_path = backdrop_path;
+        }
+
+        public boolean isAdult() {
+            return adult;
+        }
+
+        public void setAdult(boolean adult) {
+            this.adult = adult;
+        }
+
+        public String getOverview() {
+            return overview;
+        }
+
+        public void setOverview(String overview) {
+            this.overview = overview;
+        }
+
+        public String getRelease_date() {
+            return release_date;
+        }
+
+        public void setRelease_date(String release_date) {
+            this.release_date = release_date;
+        }
+
+        public List<Integer> getGenre_ids() {
+            return genre_ids;
+        }
+
+        public void setGenre_ids(List<Integer> genre_ids) {
+            this.genre_ids = genre_ids;
+        }
+    }
 }
