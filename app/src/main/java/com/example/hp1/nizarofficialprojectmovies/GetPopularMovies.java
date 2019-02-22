4de273d16b2bf91bd5631feab064fa5a5e547f97
nezarlawen1 <nezar.lawen@gmail.com>
2019-02-22 20:07:13 +0200

    popular movies api title done1

---
+++ app/src/main/java/com/example/hp1/nizarofficialprojectmovies/GetPopularMovies.java
@@ -0,0 +1,38 @@
+package com.example.hp1.nizarofficialprojectmovies;
+
+import java.io.IOException;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class GetPopularMovies {
+
+    public static String BASE_URL = "https://api.themoviedb.org";
+    public static int PAGE = 1;
+    public static String API_KEY = "efc31ce25ae8abcffb42bd206ad17519";
+    public static String LANGUAGE = "en-US";
+    public static String CATEGORY = "popular";
+    private final Api_Interface api_interface;
+
+    GetPopularMovies() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        api_interface = retrofit.create(Api_Interface.class);
+    }
+
+    void getPopularMovies (Callback<MoviesResult> moviesResultCallback) {
+
+        Call<MoviesResult> call = api_interface.listofMovies(CATEGORY, API_KEY, LANGUAGE, PAGE);
+
+        call.enqueue(moviesResultCallback);
+
+    }
+
+}
